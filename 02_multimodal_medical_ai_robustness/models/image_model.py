9fce90cdd9966f3a9a39b93006ce253a2cdfff06
Elahe Akbarnia <Elahe.akbarnia.edu@gmail.com>
2026-09-04 12:07:55 +0330

    Add SEResNet154Classifier for chest X-ray classification
    Implemented SE-ResNet-154 based classifier for chest X-ray images.

--- 02_multimodal_medical_ai_robustness/models/image_model.py
+++ 02_multimodal_medical_ai_robustness/models/image_model.py
@@ -1,1 +1,57 @@
+# SE-ResNet-154 based chest X-ray classifier.
 
+import torch
+import torch.nn as nn
+
+import timm
+
+
+
+class SEResNet154Classifier(nn.Module):
+
+
+    def __init__(
+            self,
+            pretrained=True
+    ):
+
+        super().__init__()
+
+
+        self.backbone = timm.create_model(
+            "seresnet154",
+            pretrained=pretrained,
+            num_classes=0
+        )
+
+
+        feature_dim = self.backbone.num_features
+
+
+        self.classifier = nn.Linear(
+            feature_dim,
+            1
+        )
+
+
+    def forward(
+            self,
+            x
+    ):
+
+
+        features = self.backbone(x)
+
+
+        logits = self.classifier(
+            features
+        )
+
+
+        return {
+
+            "logits": logits,
+
+            "features": features
+
+        }
