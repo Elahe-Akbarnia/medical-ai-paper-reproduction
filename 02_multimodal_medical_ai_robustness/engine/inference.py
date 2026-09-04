7a9de24ea3d66dd347e636e855587ff8f77c1346
Elahe Akbarnia <Elahe.akbarnia.edu@gmail.com>
2026-09-04 12:16:29 +0330

    Implement Predictor class for model inference
    Added a Predictor class for model inference with a predict method that returns probabilities.

--- 02_multimodal_medical_ai_robustness/engine/inference.py
+++ 02_multimodal_medical_ai_robustness/engine/inference.py
@@ -1,1 +1,42 @@
+import torch
 
+
+
+class Predictor:
+
+
+    def __init__(
+            self,
+            model,
+            device
+    ):
+
+        self.model=model
+
+        self.device=device
+
+
+
+    def predict(
+            self,
+            batch
+    ):
+
+
+        self.model.eval()
+
+
+        with torch.no_grad():
+
+
+            output=self.model(
+                **batch
+            )
+
+
+            probability=torch.sigmoid(
+                output["logits"]
+            )
+
+
+        return probability.item()
