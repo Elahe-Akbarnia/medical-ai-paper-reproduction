ae8b05b10329ea0d540f468f5986b72adaf61e5d
Elahe Akbarnia <Elahe.akbarnia.edu@gmail.com>
2026-09-04 11:44:35 +0330

    Add logger creation function
    Implement a logger creation function with file handling.

--- 02_multimodal_medical_ai_robustness/utils/logger.py
+++ 02_multimodal_medical_ai_robustness/utils/logger.py
@@ -1,1 +1,34 @@
+import logging
+from pathlib import Path
 
+
+def create_logger(
+        name: str,
+        log_file: str
+):
+
+    Path(log_file).parent.mkdir(
+        parents=True,
+        exist_ok=True
+    )
+
+
+    logger = logging.getLogger(name)
+
+    logger.setLevel(logging.INFO)
+
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(message)s"
+    )
+
+
+    file_handler = logging.FileHandler(log_file)
+
+    file_handler.setFormatter(formatter)
+
+
+    logger.addHandler(file_handler)
+
+
+    return logger
