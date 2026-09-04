1f7c73d96fd561c9bce48a074f7558374e7eb1fc
Elahe Akbarnia <Elahe.akbarnia.edu@gmail.com>
2026-09-04 12:41:40 +0330

    Implement model building function for various types

---
+++ 02_multimodal_medical_ai_robustness/src/build_model.py
@@ -0,0 +1,91 @@
+from models.image_model import (
+    SEResNet154Classifier,
+    DualViewImageModel
+)
+
+from models.text_model import (
+    BioClinicalBERTClassifier
+)
+
+from models.early_fusion import (
+    VisionBERTEarlyFusion
+)
+
+from models.late_fusion import (
+    VisionBERTLateFusion
+)
+
+from models.ensemble_fusion import (
+    VisionBERTEnsembleFusion
+)
+
+
+
+def build_model(name):
+
+
+    if name=="image":
+
+        return DualViewImageModel()
+
+
+
+    elif name=="text":
+
+        return BioClinicalBERTClassifier()
+
+
+
+    elif name=="early_fusion":
+
+
+        image_model=DualViewImageModel()
+
+        text_model=BioClinicalBERTClassifier()
+
+
+        return VisionBERTEarlyFusion(
+
+            image_model,
+
+            text_model
+
+        )
+
+
+
+    elif name=="late_fusion":
+
+
+        image_model=DualViewImageModel()
+
+        text_model=BioClinicalBERTClassifier()
+
+
+        return VisionBERTLateFusion(
+
+            image_model,
+
+            text_model
+
+        )
+
+
+
+    elif name=="ensemble":
+
+
+        return VisionBERTEnsembleFusion(
+
+            DualViewImageModel(),
+
+            BioClinicalBERTClassifier()
+
+        )
+
+
+    else:
+
+        raise ValueError(
+            f"Unknown model {name}"
+        )
