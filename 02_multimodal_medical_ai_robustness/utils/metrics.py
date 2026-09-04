44b33f2ff05d9206451448d0c037e8e0421ca5e8
Elahe Akbarnia <Elahe.akbarnia.edu@gmail.com>
2026-09-04 11:44:53 +0330

    Implement calculate_metrics function for model evaluation
    Added a function to calculate various metrics including accuracy, F1 score, precision, recall, and AUC.

--- 02_multimodal_medical_ai_robustness/utils/metrics.py
+++ 02_multimodal_medical_ai_robustness/utils/metrics.py
@@ -1,1 +1,52 @@
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    precision_score,
+    recall_score,
+    roc_auc_score
+)
 
+
+
+def calculate_metrics(
+        targets,
+        predictions,
+        probabilities=None
+):
+
+    results={}
+
+
+    results["accuracy"] = accuracy_score(
+        targets,
+        predictions
+    )
+
+
+    results["f1"] = f1_score(
+        targets,
+        predictions
+    )
+
+
+    results["precision"] = precision_score(
+        targets,
+        predictions
+    )
+
+
+    results["recall"] = recall_score(
+        targets,
+        predictions
+    )
+
+
+    if probabilities is not None:
+
+        results["auc"] = roc_auc_score(
+            targets,
+            probabilities
+        )
+
+
+    return results
