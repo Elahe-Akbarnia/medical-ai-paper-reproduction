d14e74ccc7c654a95a9685fbb4380979b896108e
Elahe Akbarnia <Elahe.akbarnia.edu@gmail.com>
2026-09-04 12:16:04 +0330

    Add Evaluator class with evaluate method
    Implement evaluation method for model predictions.

--- 02_multimodal_medical_ai_robustness/engine/evaluator.py
+++ 02_multimodal_medical_ai_robustness/engine/evaluator.py
@@ -1,1 +1,89 @@
+import torch
 
+from utils.metrics import calculate_metrics
+
+
+
+class Evaluator:
+
+
+    def __init__(
+            self,
+            model,
+            device
+    ):
+
+        self.model=model
+
+        self.device=device
+
+
+
+    def evaluate(
+            self,
+            dataloader
+    ):
+
+
+        self.model.eval()
+
+
+        targets=[]
+
+        predictions=[]
+
+        probabilities=[]
+
+
+
+        with torch.no_grad():
+
+
+            for batch in dataloader:
+
+
+                output=self.model(
+                    **batch
+                )
+
+
+                logits=output["logits"]
+
+
+
+                probs=torch.sigmoid(
+                    logits
+                )
+
+
+                preds=(
+                    probs>0.5
+                ).int()
+
+
+
+                targets.extend(
+                    batch["label"].numpy()
+                )
+
+
+                predictions.extend(
+                    preds.cpu().numpy()
+                )
+
+
+                probabilities.extend(
+                    probs.cpu().numpy()
+                )
+
+
+
+        return calculate_metrics(
+
+            targets,
+
+            predictions,
+
+            probabilities
+
+        )
